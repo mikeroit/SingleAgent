fc2698f2fcaa867d0d45ff07b31839171b54226a
mikeroit <mikeroit@gmail.com>
2016-04-08 18:05:07 -0600

    swapping now works, need to polish UndoAction

--- SAHW2/SlideTile15.cpp
+++ SAHW2/SlideTile15.cpp
@@ -54,27 +54,27 @@ void SlideTile15::GetActions(state &s, std::vector<uint8_t> &actions) {
 
 }
 
-void SlideTile15::ApplyAction(state &s, uint8_t a) {
+uint64_t swapBits(uint64_t x, uint64_t p1, uint64_t p2, uint64_t n)
+{
+    uint64_t set1 =  (x >> p1) & ((1U << n) - 1);
 
+    uint64_t set2 =  (x >> p2) & ((1U << n) - 1);
+
+    uint64_t xo = (set1 ^ set2);
+    xo = (xo << p1) | (xo << p2);
+
+    uint64_t result = x ^ xo;
+
+    return result;
+}
+
+void SlideTile15::ApplyAction(uint64_t &s, uint8_t a) {
     //fetch starting and ending locations
     int startLoc = ((a >> 4) & 0x0f); int endLoc = (a & 0x0f);
     startLoc = 60 - (startLoc * 4); endLoc = 60 - (endLoc * 4);
-
-    //fetch value at those locations
-    uint64_t fetchMask = 0xf000000000000000;
-
-    uint8_t first = (((fetchMask >> (startLoc * 4) & s) >> (60 - (startLoc * 4))));
-    uint8_t second = (((fetchMask >> (endLoc) * 4) & s) >> (60 - (endLoc * 4)));
-
-    printf("%d, %d", startLoc, endLoc);
-
-    if(! (first == 0 || second == 0)){
-        printf("Can only swap with the blank tile (tile #0)");
-    }
-
     //swap values
-    unsigned int x =  ((s >> startLoc) ^ (s >> endLoc)) & ((1U << 4) - 1);
-    s = s ^ ((x << startLoc) | (x << endLoc));
+    unsigned int x =  ((((s >> startLoc) & ((1U << 4) - 1)) ^ ((s >> endLoc) & ((1U << 4) - 1))));
+    s = swapBits(s, startLoc, endLoc, 4);
 
 }
 
